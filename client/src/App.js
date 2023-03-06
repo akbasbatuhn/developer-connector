c2ead97f17927b091bca13f549d09126615debfc
Batuhan Akbaş <batuhanakbas011@gmail.com>
2023-03-07 01:18:27 +0300

    react frontend with basic landing page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Fragment>
+        <Navbar />
+        <Landing />
+      </Fragment>
+    </div>
+  );
+};
+
+export default App;
